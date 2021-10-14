87ae2d625c1bdffc6b3802dba03fb5f727692bb0
James Hughes <jhughes1010@gmail.com>
2021-10-14 05:27:27 -0600

    y reflection added

--- pads.rb
+++ pads.rb
@@ -3,17 +3,19 @@ require 'ruby2d'
 #read coords.txt
 x_offset= 500
 y_offset= 500
+x_canvas = 1000
+y_canvas = 1000
 
 pads = File.open("coords.txt")
 
 set title: "58961"
-set width: 1000, height: 1000
+set width: x_canvas, height: y_canvas
 count = 0
 pads.each do |pad|
 p = pad.split(" ")
 
   Rectangle.new(
-    x: x_offset+p[4].to_i, y: y_offset+p[5].to_i,
+    x: x_offset+p[4].to_i, y: y_canvas - (y_offset+p[3].to_i),
     width: p[2].to_i - p[4].to_i, height: p[3].to_i - p[5].to_i,
     color: 'teal',
     z: 20
@@ -21,7 +23,7 @@ p = pad.split(" ")
 
 
 Circle.new(
-  x: x_offset + p[8].to_i, y: y_offset + p[9].to_i,
+  x: x_offset + p[8].to_i, y: y_canvas - (y_offset + p[9].to_i),
   radius: 3,
   sectors: 32,
   color: 'fuchsia',
@@ -30,11 +32,11 @@ Circle.new(
 
 
 Text.new(
-  p[1],
-  x: x_offset+p[2].to_i+5, y: y_offset+p[5].to_i+5,
+  p[0] + '-' + p[1],
+  x: x_offset+p[2].to_i+5, y: y_canvas - (y_offset+p[5].to_i + 15),
   #font: 'vera.ttf',
   style: 'bold',
-  size: 15,
+  size: 12,
   color: 'green',
   rotate: 0,
   z: 40
